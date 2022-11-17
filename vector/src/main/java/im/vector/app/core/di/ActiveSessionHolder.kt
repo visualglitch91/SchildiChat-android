e8d470f2494cbf29d23c2acea21fe113c1c6ae45
Maxime NATUREL <maxime.naturel@niji.fr>
2022-11-17 17:11:16 +0100

    Launching the sending of the client info in a dedicated coroutine to avoid ANR on application start

--- vector/src/main/java/im/vector/app/core/di/ActiveSessionHolder.kt
+++ vector/src/main/java/im/vector/app/core/di/ActiveSessionHolder.kt
@@ -112,9 +112,7 @@ class ActiveSessionHolder @Inject constructor(
                 }
                 ?: sessionInitializer.tryInitialize(readCurrentSession = { activeSessionReference.get() }) { session ->
                     setActiveSession(session)
-                    runBlocking {
-                        configureAndStartSessionUseCase.execute(session, startSyncing = startSync)
-                    }
+                    configureAndStartSessionUseCase.execute(session, startSyncing = startSync)
                 }
     }
 
