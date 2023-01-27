c47b9ee64d1d760703ade0e5612dfb2c817fd58e
SpiritCroc <dev@spiritcroc.de>
2023-01-27 10:12:18 +0100

    [TMP] Get widgets / stickers working again
    Change-Id: If24a0b93dc1d689ee68315ba068f0711c43979df

--- vector/src/main/java/im/vector/app/features/widgets/WidgetFragment.kt
+++ vector/src/main/java/im/vector/app/features/widgets/WidgetFragment.kt
@@ -92,7 +92,7 @@ class WidgetFragment :
         if (fragmentArgs.kind.isAdmin()) {
             viewModel.getPostAPIMediator().setWebView(views.widgetWebView)
         }
-        viewModel.observeViewEvents {
+        viewModel.oldObserveViewEvents {
             Timber.v("Observed view events: $it")
             when (it) {
                 is WidgetViewEvents.DisplayTerms -> displayTerms(it)
