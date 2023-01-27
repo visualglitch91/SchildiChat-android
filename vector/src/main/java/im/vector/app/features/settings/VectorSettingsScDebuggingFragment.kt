e02a3adcdfc6d7b90d7c06bb59500f47e721324e
SpiritCroc <dev@spiritcroc.de>
2023-01-27 09:00:58 +0100

    Group SC debug prefs
    Change-Id: I96ffa490c11f64da1648afe3ef545a911a350389

--- vector/src/main/java/im/vector/app/features/settings/VectorSettingsScDebuggingFragment.kt
+++ vector/src/main/java/im/vector/app/features/settings/VectorSettingsScDebuggingFragment.kt
@@ -2,6 +2,7 @@ package im.vector.app.features.settings
 
 import androidx.annotation.StringRes
 import androidx.preference.Preference
+import androidx.preference.PreferenceGroup
 import de.spiritcroc.matrixsdk.util.DbgUtil
 import im.vector.app.R
 import im.vector.app.core.preference.VectorSwitchPreference
@@ -14,29 +15,42 @@ class VectorSettingsScDebuggingFragment @Inject constructor(
     override var titleRes = R.string.settings_sc_debugging
     override val preferenceXmlRes = R.xml.vector_settings_sc_debugging
 
+    companion object {
+        const val SC_DEBUGGING_CATEGORY_LOGS = "SC_DEBUGGING_CATEGORY_LOGS"
+        const val SC_DEBUGGING_CATEGORY_VISUALS = "SC_DEBUGGING_CATEGORY_VISUALS"
+    }
+
     data class DbgPref(val key: String, @StringRes val stringRes: Int)
-    val dbgPrefs = arrayOf(
+    private val dbgLoggingPrefs = arrayOf(
             DbgPref(DbgUtil.DBG_TIMELINE_CHUNKS, R.string.settings_sc_dbg_timeline_chunks),
-            DbgPref(DbgUtil.DBG_SHOW_DISPLAY_INDEX, R.string.settings_sc_dbg_show_display_index),
             DbgPref(DbgUtil.DBG_READ_MARKER, R.string.settings_sc_dbg_read_marker),
-            DbgPref(DbgUtil.DBG_SHOW_READ_TRACKING, R.string.settings_sc_dbg_show_read_tracking),
             DbgPref(DbgUtil.DBG_READ_RECEIPTS, R.string.settings_sc_dbg_read_receipts),
             DbgPref(DbgUtil.DBG_VIEW_PAGER, R.string.settings_sc_dbg_view_pager),
+    )
+    private val dbgVisualsPrefs = arrayOf(
+            DbgPref(DbgUtil.DBG_SHOW_DISPLAY_INDEX, R.string.settings_sc_dbg_show_display_index),
+            DbgPref(DbgUtil.DBG_SHOW_READ_TRACKING, R.string.settings_sc_dbg_show_read_tracking),
             DbgPref(DbgUtil.DBG_VIEW_PAGER_VISUALS, R.string.settings_sc_dbg_view_pager_visuals),
     )
+    val dbgPrefs = dbgLoggingPrefs + dbgVisualsPrefs
 
-    override fun bindPref() {
-        dbgPrefs.forEach { dbgPref ->
+    private fun addScDbgPrefs(prefs: Array<DbgPref>, group: PreferenceGroup) {
+        prefs.forEach { dbgPref ->
             var pref: VectorSwitchPreference? = findPreference(dbgPref.key)
             if (pref == null) {
                 pref = VectorSwitchPreference(requireContext())
                 pref.key = dbgPref.key
                 pref.title = getString(dbgPref.stringRes)
-                preferenceScreen.addPreference(pref)
+                group.addPreference(pref)
             }
             pref.isChecked = DbgUtil.isDbgEnabled(pref.key)
             pref.onPreferenceChangeListener = preferenceChangeListener
         }
+    }
+
+    override fun bindPref() {
+        findPreference<PreferenceGroup>(SC_DEBUGGING_CATEGORY_LOGS)?.let { addScDbgPrefs(dbgLoggingPrefs, it) }
+        findPreference<PreferenceGroup>(SC_DEBUGGING_CATEGORY_VISUALS)?.let { addScDbgPrefs(dbgVisualsPrefs, it) }
     }
 
     private val preferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
